408402522a52eb2530920d9a0ca23f39451c3d7d
Luca Cucchetti <luca.f.cucchetti@gmail.com>
2021-03-24 19:55:28 +0000

    EJS, offline mode, dynamo

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+const RECOMMENDATIONS_TABLE = process.env.RECOMMENDATIONS_TABLE;
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  });
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+}
+
+app.use(express.json({ strict: false }));
+app.use(expressLayouts);
+
+app.set('view engine', 'ejs');
+
 // app.use(express.static('public'));
 app.get('/', function (req, res) {
-    res.sendFile( __dirname + "/html/" + "index.html" );
+  dynamoDb
+    .scan({ TableName: RECOMMENDATIONS_TABLE })
+    .promise()
+    .then((data) => {
+      console.log('Returning recommendations');
+      res.render('index', { recommendations: data.Items });
+      // res.send(html.replace('REPLACE_ME', recommendationCards(data.Items)));
+    })
+    .catch(console.error);
 });
 
-app.get('/recommend', function (req, res) {
-    res.send('Work in progress...');
+// Add recommendation endpoint
+app.post('/recommendations', function (req, res) {
+  const { title, description, author } = req.body;
+
+  const params = {
+    TableName: RECOMMENDATIONS_TABLE,
+    Item: {
+      id: uuidv4(),
+      title: title,
+      description: description,
+      author: author,
+      epoch: new Date().getTime(),
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create recommendation' });
+    }
+    res.json({ title, description, author });
+  });
 });
 
 module.exports.handler = serverless(app);
